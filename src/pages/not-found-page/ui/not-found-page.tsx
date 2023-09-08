7517183117ade17cbc0cf267a1a922e194fd6bbf
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-08 19:37:24 +0300

    feat: add styles for page-not-found

--- src/pages/not-found-page/ui/not-found-page.tsx
+++ src/pages/not-found-page/ui/not-found-page.tsx
@@ -1,15 +1,18 @@
 import pageNotFound from 'assets/not-found.png'
+import styles from './page-not-found.module.scss'
 
 export const NotFoundPage = () => {
   return (
-    <div className="page-not-found">
-      <img
-        src={ pageNotFound }
-        alt="Page Not Found"
-        height="148"
-        width="335" />
-      <h1>404 - Page Not Found</h1>
-      <p>The requested page could not be found.</p>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img
+          src={pageNotFound}
+          alt="Page Not Found"
+          className={styles.image}
+        />
+        <h1>404 - Page Not Found</h1>
+        <p>The requested page could not be found.</p>
+      </div>
     </div>
 
   )
