cf00f55aaeb4896781e3b7fbfa7c090c02330926
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-09 19:27:05 +0300

    feat: fix styles

--- src/app/providers/router/ui/app-router.tsx
+++ src/app/providers/router/ui/app-router.tsx
@@ -2,22 +2,24 @@ import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Loader } from 'shared/loader'
 import { routeConfig } from 'shared/route-config'
-import { ToolBar } from 'widgets/tool-bar/ui/tool-bar'
+import { ToolBar } from 'widgets/tool-bar'
 
 export const AppRouter = () => {
   return (
-    <Suspense fallback={ <Loader /> }>
-      <ToolBar/>
+    <Suspense fallback={<Loader />}>
       <div className="page-wrapper">
-
+        <ToolBar />
         <Routes>
-          { Object.values(routeConfig).map(({ element, path }) => (
+          {Object.values(routeConfig).map(({
+            element,
+            path
+          }) => (
             <Route
-              key={ path }
-              path={ path }
-              element={ element }
+              key={path}
+              path={path}
+              element={element}
             />
-          )) }
+          ))}
         </Routes>
       </div>
     </Suspense>
