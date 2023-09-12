107af9c2e9fd01821799f70eef4f4daacd8c55ad
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-12 21:11:40 +0300

    fix: page error message

--- src/widgets/page-error/ui/page-error.tsx
+++ src/widgets/page-error/ui/page-error.tsx
@@ -11,6 +11,7 @@ export const PageError = ({ error }: FetchingErrorProps) => {
     <div className={styles.pageError}>
       <p>Sorry.. there was an error: {error}</p>
       <button onClick={reloadPage}>Reload the Page</button>
+      <p>or check your internet connection</p>
     </div>
   )
 }
